990ff01ec1e197c1c3561b103ebba7c57bcac5bf
Davis Adaptive RF Technology Lab <dart.ucdavis@gmail.com>
2015-03-27 09:26:18 -0700

    Create s+1_s+2.py

---
+++ s+1_s+2.py
@@ -0,0 +1,23 @@
+#%matplotlib inline  # for IPython notebook
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+w=np.linspace(-10, 10, 100)
+a = np.ones((1,100),float)
+
+nom = 1j*w+a[0]
+denom =1j*w+2*a[0]
+h=nom/denom
+
+ang = np.angle(h, deg=True)
+
+#for debug
+#print(w)
+#print(nom)
+#print(denom)
+#print(h)
+#print(ang)
+
+plt.figure()
+plt.plot(w,ang)
